bd5b77a723898bce8cfec2897ba0c8b4d854e09a
LuisSe25 <luissesarego@outlook.com>
2020-09-29 19:33:46 -0500

    Mi primer commit

---
+++ src/demo/Ejecucionm.java
@@ -0,0 +1,14 @@
+package demo;
+
+public class Ejecucionm {
+
+	public static void main(String[] args) {
+	int x=1;
+	int y=2;
+	int z=3;
+	System.out.print(x);
+	System.out.print(y);
+	System.out.print(z);
+	}
+
+}
